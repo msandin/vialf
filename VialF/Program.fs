f32def4c6f6991b7ce7f2c65748d5eeb5a57bf7e
Martin Sandin <msandin@gmail.com>
2015-03-03 22:02:02 +0100

    My binding should now handle nested scopes and thus support binding through abstractions once I introduce expressions (including abstractions).

--- VialF/Program.fs
+++ VialF/Program.fs
@@ -4,9 +4,9 @@ open System
 open System.Diagnostics
 open Util
 
-type Name = Env.Name
-type Key = Env.Key
-type Path<'I> = Env.Path<'I>
+type Name = Scope.Name
+type Key = Scope.Key
+type Path<'I> = Scope.Path<'I>
 
 
 type Limit<'I> =
@@ -25,77 +25,72 @@ type Domain = {
 exception CompilerFailure of string
 
 
-let namedExternal : Def<Env.Path<Env.ScopeKey>> -> (Name * Env.ScopeKey) option =
+let namedExternal : Def<Scope.Path<Scope.ScopeKey>> -> (Name * Scope.ScopeKey) option =
   function
   | Def (name, EqLimit (path)) | Def (name, SpecLimit (path, _)) -> 
-    match Env.findExternal path with
+    match Scope.findExternal path with
     | Some ext -> Some(name, ext)
     | None -> None
 
 
-let bind : Env.ScopeKey -> Def<Path<Name>> list -> Def<Key> list =
+let bind : Scope.Scope option -> Name -> Def<Path<Name>> list -> Def<Key> list =
 
-  let externalizeDefs defs =
-    let extLimit =
-        function
-        | EqLimit path -> EqLimit (Env.externalizePath path)
-        | SpecLimit (path, args) ->
-          SpecLimit (
-            Env.externalizePath path,
-            List.map (tupleApply Env.externalizePath) args)
-    let extDef (Def(name, limit)) =
-        Def (name, extLimit limit)
-    in List.map extDef defs
+    fun parentScope scopeName defs -> 
 
-  let bindKeys scope defs =
-    let namedExts = Map.ofList (List.concat (List.map Option.toList (List.map namedExternal defs)))
-    let keyOfPath = Env.keyOfPath scope (fun name -> Map.find name namedExts)
-    let keyLimit =
-        function
-        | EqLimit path -> EqLimit (keyOfPath path)
-        | SpecLimit (path, args) -> 
-            let pathKey = keyOfPath path
-            let keyOfParam = fun paramPath ->
-                let paramName =
-                  match paramPath with
-                  | Env.LocalName name -> name
-                  | _ -> raise (CompilerFailure (sprintf "In params position: %A" paramPath))
-                match pathKey with
-                | Env.Key (Env.LocalScopeKey(_), _) -> Env.Key (scope, paramName)
-                | Env.Key (Env.ExternalScopeKey(_, _) as externalScope, _) -> Env.Key (externalScope, paramName)
-            in SpecLimit (pathKey, List.map (fun (a, b) -> (keyOfParam a, keyOfPath b)) args)
-    let keyDef (Def(name, limit)) =
-        Def (name, keyLimit limit)
-    in List.map keyDef defs
+        let scope = Scope.makeScope parentScope scopeName (List.map (function Def(name, _) -> name) defs)
 
-  fun scope defs -> bindKeys scope (externalizeDefs defs)
+        let externalizeDefs defs =
+          let extLimit =
+              function
+              | EqLimit path -> EqLimit (Scope.externalizePath path)
+              | SpecLimit (path, args) ->
+                SpecLimit (
+                  Scope.externalizePath path,
+                  List.map (tupleApply Scope.externalizePath) args)
+          let extDef (Def(name, limit)) =
+              Def (name, extLimit limit)
+          in List.map extDef defs
 
-let scope : Option<Env.Scope<Def<Key>>> -> string -> Def<Path<Name>> list -> Env.Scope<Def<Key>> =
-    fun parentScope scopeName defs ->
-        let name = Env.scopeName parentScope + scopeName
-        let scopeKey = Env.LocalScopeKey(name, Guid.NewGuid())
-        let bound = bind scopeKey defs
-        let content = List.map (function Def(name, c) as def-> (Env.Key(scopeKey,name), def)) bound
-        in { Env.key = scopeKey; Env.parent = parentScope; Env.content = Map.ofList content}
+        let bindKeys defs =
+            let namedExts = Map.ofList (List.concat (List.map Option.toList (List.map namedExternal defs)))
+            let keyOfPath path =
+                match Scope.keyOfPath scope (fun name -> Map.find name namedExts) path with
+                | Some key -> key
+                | None -> raise (CompilerFailure (sprintf "Not found: %A" path))
+            let keyLimit =
+                function
+                | EqLimit path ->  EqLimit (keyOfPath path)
+                | SpecLimit (path, args) -> 
+                    let pathKey = keyOfPath path
+                    let keyOfParam = fun paramPath ->
+                        let paramName =
+                            match paramPath with
+                            | Scope.LocalName name -> name
+                            | _ -> raise (CompilerFailure (sprintf "In params position: %A" paramPath))
+                        match pathKey with
+                        | Scope.Key (Scope.LocalScopeKey(_), _) -> Scope.Key (scope.key, paramName)
+                        | Scope.Key (Scope.ExternalScopeKey(_, _) as externalScope, _) -> Scope.Key (externalScope, paramName)
+                    in SpecLimit (pathKey, List.map (fun (a, b) -> (keyOfParam a, keyOfPath b)) args)
+            let keyDef (Def(name, limit)) =
+                Def (name, keyLimit limit)
+            in List.map keyDef defs
 
+        in bindKeys (externalizeDefs defs)
 
 // - well, refinement of existing roles (e.g. adding valueness or similar) is very much like 
 //   creating new ones and limiting them by equality... 
 // - and applying constraints in a speclimit is very much related as well...
 
-// so.. what to do... well, we'll have to build a scope... and a scope key is available to us
-
-
 [<EntryPoint>]
 let main argv = 
 
     let defs : Def<Path<Name>> list = [
-        Def ("a", EqLimit(Env.LocalName("eList")));
-        Def ("eList", EqLimit(Env.ExternalAccess("List", "list")));
-        Def ("eLength", EqLimit(Env.LocalAccess("eList", "length")));
-        Def ("dList", SpecLimit(Env.ExternalAccess("List", "list"), [(Env.LocalName("element"),Env.LocalName("a"))]));
-        Def ("dLength", EqLimit(Env.LocalAccess("dList", "length")))]
-    let result = scope None "" defs
+        Def ("a", EqLimit(Scope.LocalName("eList")));
+        Def ("eList", EqLimit(Scope.ExternalAccess("List", "list")));
+        Def ("eLength", EqLimit(Scope.LocalAccess("eList", "length")));
+        Def ("dList", SpecLimit(Scope.ExternalAccess("List", "list"), [(Scope.LocalName("element"),Scope.LocalName("a"))]));
+        Def ("dLength", EqLimit(Scope.LocalAccess("dList", "length")))]
+    let result = bind None "" defs
     Debug.Print (sprintf "Defs:\n%A" defs)
     Debug.Print (sprintf "Result:\n%A\n" result)
     0 
---
+++ VialF/Scope.fs
@@ -0,0 +1,58 @@
+﻿module Scope
+
+open System
+
+type Name = String
+
+type Path<'E> =
+    | LocalName of Name
+    | LocalAccess of Name * Name
+    | ExternalAccess of 'E * Name
+
+type ScopeKey = LocalScopeKey of Name * Guid | ExternalScopeKey of Name * Guid
+
+type Key = Key of ScopeKey * Name
+
+type Scope = {
+    key :ScopeKey
+    parent :Option<Scope>    
+    content :Set<Name>
+}
+
+let rec externalizePath : Path<Name> -> Path<ScopeKey> =
+    function
+    | LocalName name -> LocalName name
+    | LocalAccess (name, sub) -> LocalAccess (name, sub)
+    | ExternalAccess (extName, name) -> ExternalAccess (ExternalScopeKey (extName, Guid.NewGuid ()), name)
+
+let rec findExternal<'E> : Path<'E> -> Option<'E> =
+    function
+    | ExternalAccess (ext, _) -> Some ext
+    | _ -> None
+
+let scopeName :Option<Scope> -> string =
+    function
+    | None -> "/"
+    | Some(scope) -> 
+        match scope.key with
+        | LocalScopeKey (name, _) -> name
+        | ExternalScopeKey (name, _) -> "<" + name + ">"
+
+let makeScope :Scope option -> Name -> List<string> -> Scope =    
+    fun parent name fields ->
+        let scopeKey = LocalScopeKey ((scopeName parent) + name + "/", Guid.NewGuid ()) 
+        in { key = scopeKey;
+             parent = parent;
+             content = Set.ofList fields; }  
+
+let rec lookup :Scope -> Name -> Option<Key> =
+    fun scope name ->
+    if Set.contains name scope.content
+    then Some (Key(scope.key, name))
+    else Option.bind (fun parentScope -> lookup scope name) scope.parent
+
+let rec keyOfPath : Scope -> (Name -> ScopeKey) -> Path<ScopeKey> -> Key option =
+    fun scope lookupExternal -> function
+    | LocalName name -> lookup scope name
+    | LocalAccess (name, sub) -> Option.bind (fun key -> Some(key)) (lookup scope name)
+    | ExternalAccess (ext, name) -> Some(Key (ext, name))
