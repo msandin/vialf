452efccf61f74ffcc8afabeda3baa231a08a947d
Martin Sandin <msandin@gmail.com>
2015-05-17 15:29:57 +0200

    Some more stuff, I now have a very simple target that I can start targeting, if just in my mind.

--- VialF/Execute.fs
+++ VialF/Execute.fs
@@ -4,15 +4,18 @@
 type Args = (string * Exe) list
 
 and Exe = 
-  | DictCallExe of string * Args
-  | MethCallExe of Exe * Args
-  | FuncCallExe of string * Args
-  | ConstantExe of Val
+    | DictCallExe of string * Args
+    | MethCallExe of Exe * string * Args
+    | FuncCallExe of Exe * Args
+    | ConstantExe of Val
+    | FuncExe of Exe
+    | RecExe of Args
 
 and Val = 
-  | IntVal of int
-  | FuncVal of Callable
-  | ObjVal of Dict * Val
+    | IntVal of int
+    | FuncVal of Callable
+    | ObjVal of Dict * Val
+    | RecVal of Rec
 
 and Dict = Map<string, Callable>
 
@@ -20,15 +23,39 @@ and Env = {dict :Dict; values :Map<string, Val>}
 
 and Callable = Env * Exe
 
+and Rec = (string * Val) list
 
+  
 exception ExeError 
 
-let rec call : Callable -> Args -> Val =
-   fun (env, exe) args -> IntVal 8
- 
+let rec call : Callable -> Rec -> Val =
+    fun (callEnv, callExe) argRec ->
+        let bindArg values (argName, argVal) =
+            Map.add argName argVal values
+        let finalCallEnv = {
+            dict=callEnv.dict;
+            values=List.fold bindArg callEnv.values argRec}
+        in execute finalCallEnv callExe
+
+and record : Env -> Args -> Rec =
+    fun env args ->
+        List.map (fun (argName, argExe) -> (argName, execute env argExe)) args 
+
 and execute : Env -> Exe -> Val =
     fun env -> function
     | ConstantExe value -> value 
-    | DictCallExe (funName, args) -> raise ExeError
-    | MethCallExe (funName, args) -> raise ExeError
-    | FuncCallExe (funName, args) -> raise ExeError
+    | FuncExe exe -> FuncVal (env, exe)
+    | RecExe args -> RecVal (record env args)
+    | DictCallExe (funName, args) ->
+        let func = Map.find funName env.dict
+        in call func (record env args)
+    | MethCallExe (exe, methName, args) ->
+        match execute env exe with
+        | ObjVal (objDict, objVal) ->
+            let meth = Map.find methName objDict
+            in call meth (record env args)
+        | _ -> raise ExeError
+    | FuncCallExe (exe, args) -> 
+        match execute env exe with
+        | FuncVal callable -> call callable (record env args)
+        | _ -> raise ExeError
